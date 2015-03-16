b6c2a2a27126de9bdaf65c9e1281c786a1389adb
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2015-03-16 18:52:57 +0100

    implemented custom ScrollView with OnBottomReachedListener and added PrivacyNoticeActivity to show on app startup

---
+++ app/src/main/java/no/ntnu/idi/watchdogprod/PrivacyNoticeActivity.java
@@ -0,0 +1,84 @@
+package no.ntnu.idi.watchdogprod;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.AssetManager;
+import android.os.Bundle;
+import android.os.Environment;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ScrollView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
+/**
+ * Created by sigurdhf on 16.03.2015.
+ */
+public class PrivacyNoticeActivity extends Activity {
+    public static final String FILE_PRIVACY_NOTICE = "privacynotice.txt";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_privacy_notice);
+
+        Button accept = (Button) findViewById(R.id.privacy_notice_accept);
+        Button decline = (Button) findViewById(R.id.privacy_notice_decline);
+
+        accept.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent i = new Intent(PrivacyNoticeActivity.this, MainActivity.class);
+                startActivity(i);
+            }
+        });
+
+        decline.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                PrivacyNoticeActivity.this.finish();
+            }
+        });
+
+        TextView privacyNotice = (TextView) findViewById(R.id.privacy_notice_text);
+        privacyNotice.setText(getPrivacyNotice());
+
+        InteractiveScrollView scrollView = (InteractiveScrollView) findViewById(R.id.privacy_notice_scrollView);
+        scrollView.setOnBottomReachedListener(new InteractiveScrollView.OnBottomReachedListener() {
+            @Override
+            public void onBottomReached() {
+                Toast toast = Toast.makeText(PrivacyNoticeActivity.this, "Bottom reached!", Toast.LENGTH_SHORT);
+                toast.show();
+            }
+        });
+    }
+
+    private String getPrivacyNotice() {
+        StringBuilder text = new StringBuilder();
+
+        try {
+            AssetManager assetManager = this.getAssets();
+            InputStream is = assetManager.open(FILE_PRIVACY_NOTICE);
+            BufferedReader br = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String line;
+
+            while ((line = br.readLine()) != null) {
+                text.append(line);
+                text.append('\n');
+            }
+            br.close();
+        }
+        catch (IOException e) {
+            //ToDo implement error handling
+        }
+
+        return text.toString();
+    }
+}
