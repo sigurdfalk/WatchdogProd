c96281c1592a4201596000dd312d1b0766bab625
fredrikstenbro <fredrik.stenbro@hotmail.com>
2015-03-23 17:02:13 +0100

    div profil

--- app/src/main/java/no/ntnu/idi/watchdogprod/privacyProfile/Profile.java
+++ app/src/main/java/no/ntnu/idi/watchdogprod/privacyProfile/Profile.java
@@ -1,5 +1,12 @@
 package no.ntnu.idi.watchdogprod.privacyProfile;
 
+import android.content.Context;
+
+import java.util.ArrayList;
+
+import no.ntnu.idi.watchdogprod.sqlite.profile.ProfileDataSource;
+import no.ntnu.idi.watchdogprod.sqlite.profile.ProfileEvent;
+
 /**
  * Created by sigurdhf on 20.03.2015.
  */
@@ -8,6 +15,11 @@ public class Profile {
     public static final String DID_NOT_READ_EULA = "didNotReadEULA";
     public static final String INSTALLED_DANGEROUS_APP = "installedDangerousApp";
     public static final String UNINSTALLED_DANGEROUS_APP = "uninstalledDangerousApp";
+    public static final String CHANGED_SCREEN_LOCK = "changedScreenLock";
+    public static final String UNDERSTANDING_OF_PERMISSIONS = "understandingOfPermissions";
+    public static final String INTEREST_IN_PRIVACY = "interestInPrivacy";
+    public static final String UTILITY_OVER_PRIVACY = "utilityOverPrivacy";
+    public static final String CONCERNED_FOR_LEAKS = "concernedForLeaks";
 
     private double understandingOfPermissions;
     private double interestInPrivacy;
@@ -21,8 +33,26 @@ public class Profile {
         this.utilityOverPrivacy = 3.0;
     }
 
-    public double getOverallProfileScore(){
-        return -1;
+    public Profile(double understandingOfPermissions, double interestInPrivacy, double utilityOverPrivacy, double concernedForLeaks) {
+        this.understandingOfPermissions = understandingOfPermissions;
+        this.interestInPrivacy = interestInPrivacy;
+        this.concernedForLeaks = concernedForLeaks;
+        this.utilityOverPrivacy = utilityOverPrivacy;
     }
 
+    public double getUnderstandingOfPermissions() {
+        return understandingOfPermissions;
+    }
+
+    public double getInterestInPrivacy() {
+        return interestInPrivacy;
+    }
+
+    public double getUtilityOverPrivacy() {
+        return utilityOverPrivacy;
+    }
+
+    public double getConcernedForLeaks() {
+        return concernedForLeaks;
+    }
 }
