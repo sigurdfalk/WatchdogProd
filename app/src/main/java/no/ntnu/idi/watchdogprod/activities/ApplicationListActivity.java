60d11bed809685719f0d69ba7b1c969fd32e82da
Sigurd Hagen Falk <sigurd.falk@gmail.com>
2015-04-24 17:02:45 +0200

    started implementing application filtering

--- app/src/main/java/no/ntnu/idi/watchdogprod/activities/ApplicationListActivity.java
+++ app/src/main/java/no/ntnu/idi/watchdogprod/activities/ApplicationListActivity.java
@@ -1,21 +1,36 @@
 package no.ntnu.idi.watchdogprod.activities;
 
+import android.content.Context;
+import android.graphics.Point;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.support.v7.widget.DefaultItemAnimator;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.support.v7.widget.SearchView;
+import android.view.Display;
+import android.view.Gravity;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.animation.AnimationUtils;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.PopupWindow;
 
 import java.util.ArrayList;
 
+import no.ntnu.idi.watchdogprod.domain.PermissionDescription;
 import no.ntnu.idi.watchdogprod.helpers.ApplicationHelper;
 import no.ntnu.idi.watchdogprod.adapters.ApplicationListAdapter;
 import no.ntnu.idi.watchdogprod.domain.ExtendedPackageInfo;
 import no.ntnu.idi.watchdogprod.R;
+import no.ntnu.idi.watchdogprod.helpers.PermissionHelper;
+import no.ntnu.idi.watchdogprod.privacyProfile.PrivacyScoreCalculator;
 
 /**
  * Created by sigurdhf on 05.03.2015.
@@ -24,6 +39,7 @@ public class ApplicationListActivity extends ActionBarActivity {
     public static final String PACKAGE_NAME = "packageName";
 
     private ApplicationListAdapter adapter;
+    private PopupWindow popupWindow;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,7 +48,6 @@ public class ApplicationListActivity extends ActionBarActivity {
 
         ActionBar actionBar = getSupportActionBar();
         actionBar.setDisplayHomeAsUpEnabled(true);
-
         ArrayList<ExtendedPackageInfo> apps = ApplicationHelper.getThirdPartyApplications(this);
 
         RecyclerView mRecyclerView = (RecyclerView)findViewById(R.id.applications_list);
@@ -43,20 +58,66 @@ public class ApplicationListActivity extends ActionBarActivity {
         mRecyclerView.setAdapter(adapter);
     }
 
+    private void showSearchPopupWindow(View v) {
+        LayoutInflater layoutInflater = (LayoutInflater)getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        final View inflatedView = layoutInflater.inflate(R.layout.activity_application_list_search_popup, null, false);
+        populateSearchPopupWindowPermissionCheckboxes(inflatedView);
+
+        Display display = getWindowManager().getDefaultDisplay();
+        Point size = new Point();
+        display.getSize(size);
+
+        popupWindow = new PopupWindow(inflatedView, size.x,size.y - getSupportActionBar().getHeight(), true );
+        popupWindow.setBackgroundDrawable(new BitmapDrawable());
+        popupWindow.setFocusable(true);
+        popupWindow.setAnimationStyle(R.style.AnimationPopup);
+        popupWindow.showAtLocation(v, Gravity.BOTTOM, 0, 0);
+    }
+
+    private void populateSearchPopupWindowPermissionCheckboxes(View view) {
+        LinearLayout wrapper = (LinearLayout) view.findViewById(R.id.search_popup_permission_checkbox_wrapper);
+        ArrayList<PermissionDescription> permissions = PermissionHelper.getAllPermissionDescriptions(this);
+
+        for (PermissionDescription permission : permissions) {
+            if (permission.getRisk() == PrivacyScoreCalculator.RISK_HIGH) {
+                CheckBox cb = new CheckBox(this);
+                cb.setText(permission.getDesignation());
+                cb.setTextColor(getResources().getColor(R.color.text_secondary));
+                wrapper.addView(cb);
+            }
+        }
+    }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_applications, menu);
 
-        SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
-        searchView.setQueryHint(this.getString(R.string.search));
+       // SearchView searchView = (SearchView) menu.findItem(R.id.action_search).getActionView();
+       // searchView.setQueryHint(this.getString(R.string.search));
 
         return true;
     }
 
     @Override
+    public void onBackPressed() {
+        System.out.println("on back pressed");
+        if (popupWindow != null && popupWindow.isShowing()) {
+            popupWindow.dismiss();
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-        return super.onOptionsItemSelected(item);
+        switch (item.getItemId()) {
+            case R.id.action_search:
+                showSearchPopupWindow(getWindow().getDecorView().findViewById(android.R.id.content));
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
